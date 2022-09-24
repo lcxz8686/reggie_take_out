8a6276f3481f4558d13d3c28b11adc384f33a385
lcxz8686 <1585342785@qq.com>
2022-09-24 23:50:38 +0800

    初始化项目代码

--- src/main/java/com/harmony/reggie/ReggieApplication.java
+++ src/main/java/com/harmony/reggie/ReggieApplication.java
@@ -9,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class);
+        log.info("项目启动成功...");
     }
 }
